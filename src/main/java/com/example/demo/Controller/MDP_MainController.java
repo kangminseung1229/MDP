66392f545f2947cee85b11e8f6ff699230c89672
cjw1359 <45448204+cjw1359@users.noreply.github.com>
2021-07-21 09:16:44 +0900

    mdp

--- src/main/java/com/example/demo/Controller/MDP_MainController.java
+++ src/main/java/com/example/demo/Controller/MDP_MainController.java
@@ -1,12 +1,32 @@
 package com.example.demo.Controller;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.example.demo.MDP.MDP_PurchaseCode;
+import com.example.demo.MDP.mdpRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+// import com.example.demo.Service.AdminService;
+// import com.example.demo.DTO.MDP_PurchaseCode;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("MDP")
 public class MDP_MainController {
+
+
+    @Autowired
+    private mdpRepository mdpRepo;
+
     @GetMapping("/main")
     public String main(){
         return "MDP/main";
@@ -34,6 +54,46 @@ public class MDP_MainController {
     @GetMapping("/manage")
     public String manage(){
         return "MDP/manage";
+    }
+
+
+    //java random
+    @GetMapping("/random")
+    public String random(Model model, @RequestParam int count){
+
+        MDP_PurchaseCode mp = new MDP_PurchaseCode();
+
+        // int randomCHECK = 0;
+
+        Random random = new Random();
+        ArrayList<String> code = new ArrayList<String>();
+        
+        // mp.setId(1l);
+        // System.out.println(mp.getId());
+        // mp.setCode("code1234");
+        // mdpRepo.save(mp);
+        
+
+        // mp.setCode("THINKUS_53723");
+        // randomCHECK = codeRepo.randomCHECK(mp.getCode());
+
+        // model.addAttribute("randomCheck", randomCHECK);
+
+        
+
+        for(int i =0; i<count; i++){
+            mp.setId(Long.valueOf(i+1l));
+            mp.setCode("THINKUS_"+String.format("%05d",random.nextInt(100000)));
+            //select 문 필요 개수가 존재하는 여부에따라서
+
+            mdpRepo.save(mp);
+            mdpRepo.flush();
+            code.add("THINKUS_"+String.format("%05d",random.nextInt(100000)));
+        }
+        
+
+        model.addAttribute("code", code);
+        return "MDP/random";
     }
 }
 
