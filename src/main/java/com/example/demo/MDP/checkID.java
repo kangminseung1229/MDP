6840da471a72c50c9137c6eea6c05dd28bf7b06b
cjw1359 <45448204+cjw1359@users.noreply.github.com>
2021-08-20 09:05:17 +0900

    사용자 아이디 출력 수정중

--- src/main/java/com/example/demo/MDP/checkID.java
+++ src/main/java/com/example/demo/MDP/checkID.java
@@ -19,19 +19,29 @@ public class checkID {
     @Autowired
     saRepository saRepo;
 
-    //로그인 성공 확인 함수
-    public boolean checked(HttpServletRequest request, String user){
-        boolean result = false;
-        HttpSession session = request.getSession();
-        Long count=Repo.countByUserOrCodeAndUserIsNotNull(user, user);
+    // public boolean checkIdOrCode(String user){
+    //     boolean result = false;
+    //     Long count=Repo.
+        
 
+    //     return result;
+    // }
+    //로그인 성공 확인 함수
+    public String checked(HttpServletRequest request, String user){
+        String result = "false";
+        HttpSession session = request.getSession();
+        Long joinCount=Repo.countByUserOrCodeAndUserIsNotNull(user, user);
+        Long codeCount=Repo.countByCode(user);
         //로그인 성공
-        if(count >0){
+        if(joinCount >0){   
             session.setAttribute("permission", "LOGOUT");
-            result = true;            
+            result = "true";            
         }
         else {
-            session.setAttribute("permission", "LOGIN");
+            if(codeCount > 0)   //맞는 구매코드를 입력했는데 회원가입이 안되어있는 경우
+                result = "join";    
+            else
+                session.setAttribute("permission", "LOGIN");    //맞지 않은 구매코드 및 아이디를 입력한 경우
         }
         return result;
     }
@@ -44,15 +54,22 @@ public class checkID {
     }
 
     //회원가입 확인 함수
-    public boolean checkJoin(HttpServletRequest request, String user, String code){
-        boolean result = false;
-        //코드가 있는지, 해당 코드가 사용중인지, 아이디가 중복인지 확인
-        if(Repo.countByCode(code)>0 && (Repo.countByCodeAndUserIsNull(code) >0) && (Repo.countByUser(user)==0)){
-            HttpSession session = request.getSession();
-            session.setAttribute("permission", "permission");
-            result=true;
+    public String checkJoin(HttpServletRequest request, String user, String code){
+        String result = "false";
+        //코드가 있는지 
+        if(Repo.countByCode(code)>0){
+            //해당 코드가 사용중인지
+            if(Repo.countByCodeAndUserIsNull(code) >0){
+                //아이디가 중복인지 확인
+                if(Repo.countByUser(user)==0){
+                    result="true";
+                }
+                else
+                    result="overlap";
+            }   
+            else
+                result="used";
         }
-        
         return result;
     }
     //관리자 회원가입
