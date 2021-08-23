d7277eecc18035c7701c72a6160e5e059c9045a9
cjw1359 <45448204+cjw1359@users.noreply.github.com>
2021-08-23 17:36:14 +0900

    관리자 컨트롤러 분리

--- src/main/java/com/example/demo/MDP/MDP_AdminController.java
+++ src/main/java/com/example/demo/MDP/MDP_AdminController.java
@@ -1,6 +1,9 @@
 package com.example.demo.MDP;
 
-import javax.enterprise.inject.Model;
+import org.springframework.ui.Model;
+
+import java.util.Random;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -8,16 +11,23 @@ import com.example.demo.MDP.MDP_Security_DTO.SecurityAdmins;
 import com.example.demo.MDP.MDP_Security_DTO.SecurityRole;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Controller
-@RequestMapping("MDP")
+@RequestMapping("MDPadmin")
 public class MDP_AdminController {
 
+    @Autowired
+    private mdpRepository mdpRepo;
+    
     @Autowired
     private PasswordEncoder pwEncoder;
 
@@ -27,22 +37,22 @@ public class MDP_AdminController {
     @Autowired
     private checkID checkID;
     
-    @GetMapping("/adminLogin")
+    @GetMapping("/login")
     public String adminLogin(){
-        return "MDP/adminLogin";
+        return "admin/login";
     }
 
-    @GetMapping("/adminJoin")
+    @GetMapping("/join")
     public String admin_join(){
-        return "MDP/adminJoin";
+        return "admin/join";
     }
 
-    @PostMapping("/adminJoin")
+    @PostMapping("/join")
     public String admin_join(SecurityAdmins sa, String code) {
 
         //승인코드 검사
         if((code.equals("mdp2021"))==false){
-            return "redirect:adminJoin";
+            return "redirect:join";
         }
 
         //비밀번호 암호화
@@ -55,17 +65,100 @@ public class MDP_AdminController {
             sr.setId(1l);
             sa.getRoles().add(sr);
             saRepo.save(sa);
-            return "redirect:adminLogin";
+            return "redirect:login";
         }
         else{
-            return "redirect:adminJoin";
+            return "redirect:join";
         }
+    }
+
+    @GetMapping("/manage")
+    public String manage(Model model, @RequestParam(required = false, defaultValue = "") String searchText, @PageableDefault(size = 15) Pageable pageable){
+        //페이징
+        Page<mdpPurchaseCode> page = mdpRepo.findAll(pageable);
+
+        int startPage;
+        int endPage;
+
+        //한 번에 뜨는 페이지의 개수가 5개 혹은 그 미만으로 뜨게 하기 위한 조건문
+        //현재 페이지가 1,2인 경우 총 페이지가 5미만일 때는 마지막 페이지까지, 총 페이지가 5 이상일 때는 5까지 출력
+        if(page.getPageable().getPageNumber()<2){
+            startPage=1;
+            if(page.getTotalPages()<5){
+                endPage=page.getTotalPages();
+            }
+            else
+                endPage=5;
+        }
+        //현재 페이지가 3이상인 경우 첫페이지는 현재페이지-2, 마지막 페이지는 총 페이지의 개수 혹은 현재 페이지+3 중 작은 값
+        else{
+            startPage=page.getPageable().getPageNumber() - 1;
+            endPage=Math.min(page.getTotalPages(), page.getPageable().getPageNumber() + 3);
+        }
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("total", page.getTotalPages()); //총 페이지 수
+        model.addAttribute("pages", page.getPageable().getPageNumber()); // 현재 페이지
+        model.addAttribute("list", page);
+
+        return "admin/manage";
+    }
+
+    @GetMapping("/search")
+    public String search(Model model, @RequestParam(required = false, defaultValue = "") String searchText, @PageableDefault(size = 15) Pageable pageable){
+        
+        //아이디 검색
+        Page<mdpPurchaseCode> page = mdpRepo.findByUser(searchText,pageable);
+        int startPage = Math.max(1, page.getPageable().getPageNumber() - 9);
+        int endPage = Math.min(page.getTotalPages(), page.getPageable().getPageNumber() + 9);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("list", page);
+        
+        return "admin/manage";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model, @RequestParam int count){
+        mdpPurchaseCode mp = new mdpPurchaseCode();
+
+        Random random = new Random();
+        long last=mdpRepo.lastColumn();
+
+        for(int i=0; i<count; i++){
+            String pc = "THINKUS_";
+            mp.setId(Long.valueOf(i+last+1l)); //id 설정
+            for(int j=0; j<5; j++){
+                int rd=random.nextInt(2);
+                char ch=(char)((Math.random()*26)+65);
+                if(rd==1){
+                    pc=pc.concat(Integer.toString(random.nextInt(10)));
+                }
+                else{
+                    pc=pc.concat(Character.toString(ch));
+                }
+            }
+            mp.setCode(pc);
+            
+            //중복 제거
+            if(mdpRepo.countByCode(mp.getCode())>0){
+                i--;
+                continue;
+            }
+            
+            mdpRepo.save(mp);
+            mdpRepo.flush();
+        }
+
+        return "redirect:manage";
+        
     }
 
     @GetMapping("/adminLogout")
     public String adminLogout(HttpServletRequest request, Model model){
         HttpSession session = request.getSession();
         session.invalidate();
-        return "MDP/adminLogin";        
+        return "admin/login";        
     }
 }
--- src/main/java/com/example/demo/MDP/MDP_MainController.java
+++ src/main/java/com/example/demo/MDP/MDP_MainController.java
@@ -2,28 +2,18 @@ package com.example.demo.MDP;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.xml.registry.infomodel.User;
-
-import com.example.demo.MDP.MDP_Security_DTO.SecurityAdmins;
-import com.example.demo.MDP.MDP_Security_DTO.SecurityRole;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("MDP")
@@ -259,89 +249,7 @@ public class MDP_MainController {
         return "MDP/fin";
     }
 
-    @GetMapping("admin/manage")
-    public String manage(Model model, @RequestParam(required = false, defaultValue = "") String searchText, @PageableDefault(size = 15) Pageable pageable){
-
-        //페이징
-        Page<mdpPurchaseCode> page = mdpRepo.findAll(pageable);
-
-        int startPage;
-        int endPage;
-
-        //한 번에 뜨는 페이지의 개수가 5개 혹은 그 미만으로 뜨게 하기 위한 조건문
-        //현재 페이지가 1,2인 경우 총 페이지가 5미만일 때는 마지막 페이지까지, 총 페이지가 5 이상일 때는 5까지 출력
-        if(page.getPageable().getPageNumber()<2){
-            startPage=1;
-            if(page.getTotalPages()<5){
-                endPage=page.getTotalPages();
-            }
-            else
-                endPage=5;
-        }
-        //현재 페이지가 3이상인 경우 첫페이지는 현재페이지-2, 마지막 페이지는 총 페이지의 개수 혹은 현재 페이지+3 중 작은 값
-        else{
-            startPage=page.getPageable().getPageNumber() - 1;
-            endPage=Math.min(page.getTotalPages(), page.getPageable().getPageNumber() + 3);
-        }
-
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
-        model.addAttribute("total", page.getTotalPages()); //총 페이지 수
-        model.addAttribute("pages", page.getPageable().getPageNumber()); // 현재 페이지
-        model.addAttribute("list", page);
-        
-        return "MDP/manage";
-    }
-
-    @GetMapping("admin/search")
-    public String search(Model model, @RequestParam(required = false, defaultValue = "") String searchText, @PageableDefault(size = 15) Pageable pageable){
-        
-        //아이디 검색
-        Page<mdpPurchaseCode> page = mdpRepo.findByUser(searchText,pageable);
-        int startPage = Math.max(1, page.getPageable().getPageNumber() - 9);
-        int endPage = Math.min(page.getTotalPages(), page.getPageable().getPageNumber() + 9);
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
-        model.addAttribute("list", page);
-        
-        return "MDP/manage";
-    }
-
-    @GetMapping("admin/add")
-    public String add(Model model, @RequestParam int count){
-        mdpPurchaseCode mp = new mdpPurchaseCode();
-
-        Random random = new Random();
-        long last=mdpRepo.lastColumn();
-
-        for(int i=0; i<count; i++){
-            String pc = "THINKUS_";
-            mp.setId(Long.valueOf(i+last+1l)); //id 설정
-            for(int j=0; j<5; j++){
-                int rd=random.nextInt(2);
-                char ch=(char)((Math.random()*26)+65);
-                if(rd==1){
-                    pc=pc.concat(Integer.toString(random.nextInt(10)));
-                }
-                else{
-                    pc=pc.concat(Character.toString(ch));
-                }
-            }
-            mp.setCode(pc);
-            
-            //중복 제거
-            if(mdpRepo.countByCode(mp.getCode())>0){
-                i--;
-                continue;
-            }
-            
-            mdpRepo.save(mp);
-            mdpRepo.flush();
-        }
-
-        return "redirect:manage";
-        
-    }
+    
 
 
     
