a1603b59f67f21f32d89e04695e2366902ac36ca
cjw1359 <45448204+cjw1359@users.noreply.github.com>
2021-08-25 13:16:42 +0900

    관리자 페이지 수정

--- src/main/java/com/example/demo/Controller/MainController.java
+++ src/main/java/com/example/demo/Controller/MainController.java
@@ -1,15 +1,26 @@
 package com.example.demo.Controller;
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController{
 
     @GetMapping("/")
-    public String index() {
-        return "MDP/main";      
+    public String index(HttpServletRequest request, Model model) {
+        // 세션 검사
+        HttpSession session = request.getSession();
+        String permission = (String) session.getAttribute("permission");
+        String user = (String) session.getAttribute("user");
+        model.addAttribute("loginOut", permission);
+        model.addAttribute("user", user);
+
+        return "MDP/main";     
     }
 
 }
